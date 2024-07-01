27478b7e33ecb27ea1bed615e7803756f67fcd39
divyasoni25 <143200499+divyasoni25@users.noreply.github.com>
2024-07-01 21:57:51 +0530

    updates
    Updates

--- building_model.py
+++
@@ -1,9 +0,0 @@
-tf.reset_default_graph()
-
-net = tflearn.input_data(shape = [None, len(training[0])])
-net = tflearn.fully_connected(net,8)
-net = tflearn.fully_connected(net,8)
-net = tflearn.fully_connected(net,len(output[0]), activation = "softmax")
-net = tflearn.regression(net)
-
-model = tflearn.DNN(net)
--- train_model.py
+++
@@ -1,3 +0,0 @@
-
-model.fit(training, output, n_epoch = 200, batch_size = 8, show_metric = True)
-model.save("model.tflearn")
